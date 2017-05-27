e29de617592722f2255826db0975ce15d3f3311a
P4ndemic <riccardovarenna@hotmail.de>
2017-05-27 21:12:56 +0000

    changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+
+// port ajusted to environment
 var port = process.env.PORT
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
