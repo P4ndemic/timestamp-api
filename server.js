cb754a821b074973f45a989a7c0a7d8c724bfcf4
P4ndemic <riccardovarenna@hotmail.de>
2017-05-29 15:24:59 +0000

    moment.js

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require('express')
 var app = express()
 var url = require("url")
 var http = require("http")
+var path = require("path")
+var moment = require("moment")
 
 // port ajusted to environment
 var port = process.env.PORT
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"))
+})
 
 app.get("/:date", function (req, res) {
-  res.send(req.params.date)
+  var date = req.params.date
+  res.send(moment(date).format("MMMM DD YYYY"))
 })
 
 app.listen(port, function () {
