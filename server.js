84579f35b2b602b5ba3733bc29b958f200dfb13e
P4ndemic <riccardovarenna@hotmail.de>
2017-05-29 16:40:27 +0000

    wasd

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-var express = require('express')
-var app = express()
-var url = require("url")
-var path = require("path")
-var moment = require("moment")
+var express = require('express');
+var app = express();
+var path = require("path");
+var moment = require("moment");
 
 // port ajusted to environment
-var port = process.env.PORT
+var port = process.env.PORT;
 
 app.all("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"))
-})
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.all("/:date", function (req, res) {
-  var date = req.params["date"]
-  var unix = null
-  var natural = null
-  var regex = /^[0-9]*$/g
+  var date = req.params["date"];
+  var unix = null;
+  var natural = null;
+  var regex = /^[0-9]*$/g;
   if(regex.test(date)) {
-    unix = date
-    natural = moment.unix(date).format("MMMM DD YYYY")
+    unix = date;
+    natural = moment.unix(date).format("MMMM DD YYYY");
   } else if (moment(date, "MMMM DD YYYY").isValid()) {
-    unix = moment(date).format("X")
-    natural = moment(date).format("MMMM DD YYYY")
+    unix = moment(date).format("X");
+    natural = moment(date).format("MMMM DD YYYY");
   } else {
-    unix = null
-    natural = null
+    unix = null;
+    natural = null;
   }
-  res.send(date)
-  // res.send(JSON.stringyfy({
-  //   "unix": unix,
-  //   "natural": natural
-  // }))
-})
+  res.send(JSON.stringyfy({
+    "unix": unix,
+    "natural": natural
+  }));
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port' + port)
+  console.log('Example app listening on port' + port);
 })
